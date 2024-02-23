b26ca1d01ff08361214ec415a027c6d996116fb6
Dvs-uday <dvsuday@gmail.com>
2024-02-23 20:53:14 +0530

    starting

---
+++ Eis-prac/Sample-eis/src/main/java/com/eisdemo/Sampleeis/SampleEisApplication.java
@@ -0,0 +1,13 @@
+package com.eisdemo.Sampleeis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEisApplication.class, args);
+	}
+
+}
---
+++ Eis-prac/Sample-eis/src/test/java/com/eisdemo/Sampleeis/SampleEisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eisdemo.Sampleeis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleEisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
